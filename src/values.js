3a8557afca135a66e2931cbc16a5b5cb80a9b9fe
Ridwan Olalere <ridwan@Ridwans-MacBook-Pro.local>
2016-07-17 16:33:54 +0100

    authentication class

---
+++ src/values.js
@@ -0,0 +1,15 @@
+/*
+* This file contains static string values
+* used in varius path of the application
+*
+*/
+var values = {
+  server_base_url: {
+    staging: "https://pwcstaging.herokuapp.com",
+    production: "https://pwcstaging.herokuapp.com"
+  },
+  authentication_path: "/oauth/token",
+  payment_path: "/orders/oneOffPayment"
+}
+
+module.exports = values;
