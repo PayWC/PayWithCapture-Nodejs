d0a7c36425129714b4d1869cf15c142b7e78f0f3
Ridwan Olalere <ridwan@Ridwans-MacBook-Pro.local>
2016-07-19 13:00:08 +0100

    card-payment-tutorial

---
+++ index.js
@@ -0,0 +1,31 @@
+var AccountPayment = require('./src/account-payment');
+var CardPayment = require('./src/card-payment');
+var QrCode = require('./src/qr-code');
+var Otp = require('./src/otp');
+var PosPrinting = require('./src/pos-printing');
+
+var PayWithCaptureClient = function(_clientId, _clientSecret, _env) {
+  var clientId = _clientId,
+      clientSecret = _clientSecret,
+      env = _env;
+
+  this.getAccountPayment = function() {
+    return new AccountPayment(clientId, clientSecret, env);
+  }
+
+  this.getCardPayment = function() {
+    return new CardPayment(clientId, clientSecret, env);
+  }
+
+  this.getQrCode = function() {
+    return new QrCode(clientId, clientSecret, env);
+  }
+
+  this.getOtp = function() {
+    return new Otp(clientId, clientSecret, env);
+  }
+
+  this.PosPrinting = function() {
+    return new PosPrinting(clientId, clientSecret, env);
+  }
+}
