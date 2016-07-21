55c8d8274ac3e8277a01a78950a21858c3be3bc5
Ridwan Olalere <ridwan@Ridwans-MacBook-Pro.local>
2016-07-21 15:08:50 +0100

    index.js with authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var CardPayment = require('./src/card-payment');
 var QrCode = require('./src/qr-code');
 var Otp = require('./src/otp');
 var PosPrinting = require('./src/pos-printing');
+var Authentication = require('./src/authentication');
 
 var PayWithCaptureClient = function(_clientId, _clientSecret, _env) {
   var clientId = _clientId,
@@ -27,6 +28,11 @@ var PayWithCaptureClient = function(_clientId, _clientSecret, _env) {
 
   this.PosPrinting = function() {
     return new PosPrinting(clientId, clientSecret, env);
+  }
+
+  
+  this.getAuthentication = function() {
+    return new Authentication(clientId, clientSecret, env);
   }
 }
 
