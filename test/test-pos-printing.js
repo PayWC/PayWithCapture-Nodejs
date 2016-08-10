a18c41948b90e3c33e37621683626110f9664eaa
Adeoye Remi <mail4remi@gmail.com>
2016-08-10 12:40:27 +0100

    Updated and fixed tests

--- test/test-pos-printing.js
+++ test/test-pos-printing.js
@@ -2,18 +2,23 @@ var assert = require('chai').assert;
 var PosPrinting = require('../src/pos-printing');
 
 describe("PosPrinting", function() {
+
   describe("#printFromMerchantCode()", function() {
+    this.timeout(15000); //Fails with default. Default set at 2000ms
+
     var printedTransaction;
+    var clientId = "577e5fe42989c31100b26f14";
+    var clientSecret = "diHopa8yFNDWofRNJIeREDmAV3HhL7bwr4umhlhPS0CgqIiOylA6Y9obfsV9VsbWBDuMUKE7MvVpIrtip4oX8zmG21I4QI1rhwjx";
+
+    var code = "6836006";
+    var printingClient;
 
     before(function() {
-      var clientId = "577e5fe42989c31100b26f14";
-      var clientSecret = "diHopa8yFNDWofRNJIeREDmAV3HhL7bwr4umhlhPS0CgqIiOylA6Y9obfsV9VsbWBDuMUKE7MvVpIrtip4oX8zmG21I4QI1rhwjx";
-      var printingClient = new PosPrinting(clientId, clientSecret, "staging");
-      var code = "6836006"
+      printingClient = new PosPrinting(clientId, clientSecret, "staging");
       return printingClient.printFromMerchantCode(code)
-                    .then(function(resp) {
-                      printedTransaction = resp;
-                    });
+              .then(function(resp) {
+                printedTransaction = resp;
+              });
     });
 
     it("printFromMerchantCode response status should be success", function() {
